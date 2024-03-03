b6e38a77809efb7e45be8ebef309d7a4ae330cc1
pysan3 <pysan3@gmail.com>
2024-03-03 11:47:08 +0000

    fix(wm): make neo-tree handle previous buffer

--- lua/neo-tree/manager/current.lua
+++ lua/neo-tree/manager/current.lua
@@ -2,31 +2,15 @@ local NuiSplit = require("nui.split")
 local buf_storage = require("nui.utils.buf_storage")
 local utils = require("nui.utils")
 
----@class nt_current_internal : nui_split_internal
----@field prev_buf integer|nil
-
 ---@class NeotreeCurrentWin : NuiSplit
----@field private _ nt_current_internal
-local M = NuiSplit.static.extend(NuiSplit, "NeotreeCurrentWin") ---@diagnostic disable-line
-
-function M:_get_prev_buf()
-  local new_buf = self._.prev_buf
-  if new_buf and vim.api.nvim_buf_is_valid(new_buf) then
-    return new_buf
-  end
-  new_buf = vim.fn.bufnr("#")
-  if new_buf >= 1 then
-    return new_buf
-  end
-  return vim.api.nvim_create_buf(true, false)
-end
+---@field private _ nui_split_internal
+local M = NuiSplit:extend("NeotreeCurrentWin") ---@diagnostic disable-line
 
 function M:_open_window()
   if self.winid or not self.bufnr then
     return
   end
 
-  self._.prev_buf = vim.api.nvim_get_current_buf()
   self.winid = vim.api.nvim_get_current_win()
 
   vim.api.nvim_win_set_buf(self.winid, self.bufnr)
@@ -43,10 +27,6 @@ function M:_close_window()
     return
   end
 
-  if vim.api.nvim_win_is_valid(self.winid) and not self._.pending_quit then
-    vim.api.nvim_win_set_buf(self.winid, self:_get_prev_buf())
-  end
-
   self.winid = nil
 end
 
@@ -56,9 +36,7 @@ function M:_buf_destroy()
       utils._.clear_namespace(self.bufnr, self.ns_id)
 
       if not self._.pending_quit then
-        vim.api.nvim_win_set_buf(self.winid, self:_get_prev_buf())
-        -- TODO: No need to delete the buffer? Test it out.
-        -- vim.api.nvim_buf_delete(self.bufnr, { force = true })
+        vim.api.nvim_buf_delete(self.bufnr, { force = true })
       end
     end
 
